46d4b856f9c905ed3effdb36340a37783ff24094
Binoj Vijayakumar <binoj.claysys@gmail.com>
2018-08-10 16:08:19 +0530

    add custom headers

--- JS/getDocDetails.js
+++ JS/getDocDetails.js
@@ -1,4 +1,4 @@
-function getDocDetails(apiURL, contetntType, taxProcess, Jurisdiction, itemID, ctrlid, samName) {
+function getDocDetails(apiURL, contetntType, taxProcess, Jurisdiction, itemID, ctrlid, clientKey, userName) {
     var apiData = {
         "SortByColumn": "DocumentID",
         "SortType": "DESC",
@@ -17,24 +17,19 @@ function getDocDetails(apiURL, contetntType, taxProcess, Jurisdiction, itemID, c
         }],
         "ModuleName": "Documents"
     };
+    var custom_headers = {};
+    custom_headers['CSKII'] = clientKey;
+    custom_headers['CSKII_UPN'] = userName;
     $.ajax({
         type: "POST",
         url: apiURL,//"//qa4svc.apps.tax/api/documents/GetDocumentsFromDB/1/",
-        headers: {
-            'CSKII': '35bba0d9-f7e5-413a-8815-d2945c55b169',
-            'CSKII_UPN': samName
-        },
+        headers: custom_headers,
         data: JSON.stringify(apiData),
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         xhrFields: {
             withCredentials: true
         },
-        // beforeSend: function(xhr) {
-        //     xhr.setRequestHeader("Authorization", getAuthCookie());
-        // },
-        // username: "taxcloud\\nithapaul",
-        // password: "virtual_2017",
 
         success: function (response) {
             $('#' + ctrlid).val(JSON.stringify(response));
@@ -42,18 +37,3 @@ function getDocDetails(apiURL, contetntType, taxProcess, Jurisdiction, itemID, c
         }
     });
 }
-
-// function getAuthCookie() {
-//     debugger;
-//     var cn = "ASP.NET_SessionId=";
-//     // var cn = "Authorization=";
-//     var idx = document.cookie.indexOf(cn)
-
-//     if (idx != -1) {
-//         var end = document.cookie.indexOf(";", idx + 1);
-//         if (end == -1) end = document.cookie.length;
-//         return unescape(document.cookie.substring(idx + cn.length, end));
-//     } else {
-//         return "";
-//    }
-//  }
