78518d099ba154312c1aaa23d0eafc46e593fd37
Binoj Vijayakumar <binoj.claysys@gmail.com>
2018-12-13 17:12:40 +0530

    darkerDisabledFont

---
+++ JS/darkerDisabledFont.js
@@ -0,0 +1,3 @@
+$(function () {
+    $('<style>input[type=button][disabled], input[type=reset][disabled], input[type=submit][disabled], button[disabled], input[type=password][disabled], input[type=text][disabled], input[type=file][disabled], textarea[disabled], select[disabled]{color:#565656!important;}</style>').appendTo('head');
+});
