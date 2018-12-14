06d347ea647803d561f4a0da088c6775c05d2532
Binoj Vijayakumar <binoj.claysys@gmail.com>
2018-12-14 15:54:40 +0530

    disabled colour darker - add textarea

--- JS/darkerDisabledFont.js
+++ JS/darkerDisabledFont.js
@@ -1,3 +1,3 @@
 $(function () {
-    $('<style>input[type=button][disabled], input[type=reset][disabled], input[type=submit][disabled], button[disabled], input[type=password][disabled], input[type=text][disabled], input[type=file][disabled], textarea[disabled], select[disabled]{color:#565656!important;}</style>').appendTo('head');
+    $('<style>input[type=button][disabled], input[type=reset][disabled], input[type=submit][disabled], button[disabled], input[type=password][disabled], input[type=text][disabled], input[type=file][disabled], textarea[disabled], select[disabled], .disabled_textarea_style{color:#565656!important;}.disabled_textarea_style{opacity:1!important;}</style>').appendTo('head');
 });
