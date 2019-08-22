92147f406823c0647f2831c204e1b7e05a243674
Binoj Vijayakumar <binojvijayakumar@gmail.com>
2019-08-22 15:09:15 +0530

    code updates

---
+++ JS/mrm_style_tabs.js
@@ -0,0 +1,17 @@
+function mrm_styleTabs_top(ctrlid, activeColor, borderColor) {
+    if (ctrlid && ctrlid.split('_').length && ctrlid.split('_').length == 3) {
+        $('#mrm_style_Tabs_top').remove();
+        $(`<style id="mrm_style_Tabs_top">
+        #` + ctrlid + `_TabHead>ul>li,#` + ctrlid + `_TabHead>ul>li>a{background-color:transparent!important;border:none;}
+        #` + ctrlid + `_TabHead>ul>li>a{padding:0;}
+        #` + ctrlid + `_TabHead .ui-tabs-active{border-bottom: 3px solid ` + activeColor + ` !important;box-sizing:border-box;}
+        #` + ctrlid + `_tabItems>div{border:none!important;border-top:1px solid ` + borderColor + `!important;box-shadow: none!important;}
+        #` + ctrlid + ` div.clcontrol-form{margin-left:-2px!important;}
+        </style>`).appendTo('head');
+    }
+    // $(document).on('click', '#' + ctrlid + '_TabHead a', function (event) {
+
+    //     // Stop default browser action which would likely return to the top of the page
+    //     event.preventDefault();
+    // });
+}
