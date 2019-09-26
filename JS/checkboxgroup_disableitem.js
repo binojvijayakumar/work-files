4901c5bfcd075f18fc09a59f05c73a23e704b007
Binoj Vijayakumar <binojvijayakumar@gmail.com>
2019-09-26 19:02:52 +0530

    value check against unicode

--- JS/checkboxgroup_disableitem.js
+++ JS/checkboxgroup_disableitem.js
@@ -2,8 +2,16 @@ var disablecheckboxgroupitem = function (ctrlid, values, separator, state) {
     state = state === 'true';
     if (ctrlid && values && separator) {
         var valueArr = values.split(separator);
-        $.each(valueArr, function (i, v) {
-            $('#' + ctrlid + ' input[type="checkbox"][value="' + v + '"]').prop('disabled', state);
+        // $.each(valueArr, function (i, v) {
+        //     $('#' + ctrlid + ' input[type="checkbox"][value="' + v + '"]').prop('disabled', state);
+        // });
+
+        $('#' + ctrlid + ' input[type="checkbox"]').each(function () {
+            if ($.inArray($(this).attr('value').replace(/\\u[\dA-F]{4}/gi, function (match) {
+                return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+            }), valueArr) > -1) {
+                $(this).prop('disabled', state);
+            }
         });
     }
 }
